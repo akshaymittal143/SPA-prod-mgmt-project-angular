4da52a66abd9d1d7fa94c2b2f00504d9281e4b16
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-09 22:10:13 -0500

    render product list view on main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component} from '@angular/core';
 @Component({
   selector: 'pm-root',
   template:` 
-  <div><h1>{{pageTitle}}</h1>
-    <div>My First Component</div>
-   </div> 
+  <div><h1>{{pageTitle}}</h1></div>
+    <pm-products></pm-products>
   `
-  
+
 })
 
 export class AppComponent{
