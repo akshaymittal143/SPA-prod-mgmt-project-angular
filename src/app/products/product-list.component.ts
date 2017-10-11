1725d939fefafd85efa20ec186b0cb2718a7be9b
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-10 22:12:06 -0500

    Intiate data binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import {Component} from '@angular/core'
 
 export class ProductListComponent{
     pageTitle: string = 'Product List';
+    imageWidth: number=50;
+    imageMargin: number=2;
+    showImage: boolean= false;
     products: any[] = [
         {
             "productId": 1,
@@ -28,5 +31,8 @@ export class ProductListComponent{
             "starRating": 4.2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         }
-    ]
+    ];
+    toggleImage(): void{
+        this.showImage=!this.showImage;
+    }
 }
