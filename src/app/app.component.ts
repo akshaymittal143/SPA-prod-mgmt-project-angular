eaa6facb8007e32e56e597d544037f4db096a72b
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-11 22:24:52 -0500

    registering the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component} from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
   template:` 
   <div><h1>{{pageTitle}}</h1>
     <pm-products></pm-products>
-    </div>`
+    </div>`,
+    providers: [ProductService]
 
 })
 
