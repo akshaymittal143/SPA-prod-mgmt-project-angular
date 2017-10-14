95907be03545c1b342b81a730b3bc157fe63d8a6
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-13 20:47:04 -0500

    Implement routing with home page and product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,13 @@ import { WelcomeComponent } from './home/welcome.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
