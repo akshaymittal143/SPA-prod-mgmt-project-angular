e6607eea550a653da453501d2f5fd67adef85da0
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-14 13:00:04 -0500

    added back button p-detail page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 import {IProduct} from './product';
 @Component({
@@ -10,9 +11,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    let id= +this._route.snapshot.paramMap.get('id');
+    this.pageTitle+= `: ${id}`;
+    this.product={
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+  }
+
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
