7942cb2c066e498a17c17fc39d2e54e587b3b778
Akshay Mittal <akshaycanodia@gmail.com>
2017-10-13 19:51:14 -0500

    added exception handling

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import { IProduct } from './product';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do'
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Injectable()
@@ -11,7 +15,14 @@ export class ProductService{
 
     constructor(private _http: HttpClient) {}
     getProducts(): Observable<IProduct[]>{
-      return this._http.get<IProduct[]>(this._productUrl);
+      return this._http.get<IProduct[]>(this._productUrl)
+      .do(data=>console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+    }
+
+    private handleError(err: HttpErrorResponse){
+        console.log(err.message);
+        return Observable.throw(err.message);
     }
 
 }
